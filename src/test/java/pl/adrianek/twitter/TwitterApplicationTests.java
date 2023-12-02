f8ac613842285cb362686c7c625f517bd25fccdd
BuildTools <unconfigured@null.spigotmc.org>
2023-12-02 12:41:00 +0100

    ?

---
+++ src/test/java/pl/adrianek/twitter/TwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adrianek.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
